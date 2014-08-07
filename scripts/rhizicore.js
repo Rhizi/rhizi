9a2f502c5f2337c5b03f11a9e7bd0a421798430e
Owen <despito@MacBook-Pro-de-Despito.local>
2014-08-07 16:32:19 +0300

    Issue 21

--- scripts/rhizicore.js
+++ scripts/rhizicore.js
@@ -425,7 +425,7 @@ function myGraph(el) {
             .text(function(d) {
 
                 if ( d.name && d.target.state === "temp" || d.source.state === "chosen" || d.target.state === "chosen") {
-                    if (d.name.length < 25) {
+                    if (d.name.length < 25 || d.source.state === "chosen" || d.target.state === "chosen") {
                         return d.name;
                     } else {
                         return d.name.substring(0, 14) + "...";
