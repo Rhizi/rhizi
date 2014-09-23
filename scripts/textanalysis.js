Rhizi/rhizi

commit af27835be74279fe894318fe65d097ddff7384b0

textanalysis: multiple changes to autocomplete
1. move the sugg variable to textanalysis.js instead of the ui part
2. use a callback for the autocomplete widget (it supports three
options: array, string as url for a json returning service, or callback)
3. use a dictionary instead of an array to keep the list of suggestions.

We iterate on the dictionary instead of using a Trie or some other
datastructure in the callback. But the dictionary avoids iteration when
adding a word. Overall a loss in performance, but not noticeable.

Googling / memory suggests a Trie is what we want here, but I'm
postponing that for later. See:

http://ejohn.org/blog/javascript-trie-performance-analysis

scripts/textanalysis.js
@@ -10,8 +10,35 @@ var ExecutionStack = [];
 
 var lastnode;
 
+var sugg = {}; // suggestions for autocompletion of node names
+
 var ANALYSIS_NODE_START = 'ANALYSIS_NODE_START';
 var ANALYSIS_LINK = 'ANALYSIS_LINK';
+
+function autoSuggestAddName(name)
+{
+    console.log('adding suggestion ' + name);
+    /* note that name can contain spaces - this is ok. We might want to limit this though? */
+    if(name.split(" ").length > 1) {
+        sugg['"'+newnodes[n]+'"'] = 1;
+    } else {
+        sugg[name] = 1;
+    }
+}
+
+function autocompleteCallback(request, response_callback)
+{
+    console.log('autocompleteCallback: ' + request.term);
+    var ret = [];
+    if (request.term === "" || request.term) {
+        for (var name in sugg) {
+            if (name.indexOf(request.term) == 0) {
+                ret.push(name);
+            }
+        }
+    }
+    response_callback(ret);
+}
 
 function TextAnalyser2(newtext, finalize) {
     var segment = [],
@@ -130,19 +157,9 @@ function TextAnalyser2(newtext, finalize) {
     var typesetter = "";
     if (finalize === true) {
         typesetter = "perm";
-        var sugg = []
         for (var n = 0; n < newnodes.length; n++) {
-            if (Unique(newnodes[n])) {
-                if(newnodes[n].split(" ").length>1){
-                    sugg.push('"'+newnodes[n]+'"');
-                }else{
-                    sugg.push(newnodes[n]);
-                }
-                
-            }
+            autoSuggestAddName(newnodes[n]);
         }
-        sugg.push(text);
-        ret.sugg = sugg;
     } else {
         typesetter = "temp";
     }
@@ -245,16 +262,4 @@ function TextAnalyser2(newtext, finalize) {
     lastnode = newnodes[nodeindex];
 
     return ret;
-}
-
-
-
-function Unique(newnode) {
-    var truth = true;
-    for (var p = 0; p < sugg.length; p++) {
-        if (sugg[p] === newnode) {
-            truth = false;
-        }
-    }
-    return truth;
 }

scripts/textanalysis-ui.js
@@ -1,19 +1,15 @@
 var text = ""; // Last text of sentence
-var sugg = []; // suggestions for autocompletion of node names
 
 $('#textanalyser').autocompleteTrigger({
     triggerStart: '#',
     triggerEnd: '',
-    source: sugg
+    source: autocompleteCallback
 });
 
 // TODO - hide this in a scope
 function analyzeSentence(sentence, finalize)
 {
     var ret = TextAnalyser2(sentence, finalize);
-    for (var k in ret.sugg) {
-        sugg.push(ret.sugg[k]);
-    }
     switch (ret.state) {
     case ANALYSIS_NODE_START:
         $('.typeselection').css({top:window.innerHeight/2-115,left:window.innerWidth/2-325});

commit b0dc9ba6372735708e34a42c81e20e29d44dbc76

fix a #50 noticed error (introduced in af27835b)

scripts/textanalysis.js
@@ -19,7 +19,7 @@ function autoSuggestAddName(name)
 {
     /* note that name can contain spaces - this is ok. We might want to limit this though? */
     if(name.split(" ").length > 1) {
-        sugg['"'+newnodes[n]+'"'] = 1;
+        sugg['"' + name + '"'] = 1;
     } else {
         sugg[name] = 1;
     }