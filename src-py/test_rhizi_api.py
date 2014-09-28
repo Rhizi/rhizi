e65ca88e90c0828d46e4b58580695ee14c897086
LV-426 <lv-426@taproot.org.il>
2014-09-28 22:22:59 +0300

    rm 'self.test_client' in favor of 'with ...' statements

--- src-py/test_rhizi_api.py
+++ src-py/test_rhizi_api.py
@@ -11,30 +11,41 @@ class TestRhiziAPI(unittest.TestCase):
         cfg = Config.init_from_file('res/etc/rhizi-server.conf')
         db_ctl = dbc.DB_Controller(cfg)
         rhizi_api.db_ctl = db_ctl
-        self.webapp = rhizi_api.webapp.test_client()
+
+    def test_add_node_set(self):
+        """
+        add node set test
+        """
+        node_map = { 'Skill': { 'name': 'kung-fu' } }
+        
+        with rhizi_api.webapp.test_client() as c:
+            req = c.post('/add/node-set', data={'node_map':node_map})
+            rz_data = json.loads(req.data)['data']
 
     def test_load_node_non_existing(self):
         """
-        test loading a non existing node
+        loading a non existing node test
         """
         node_id = 'non_existing_id'
-        rv = self.webapp.post('/load/node-single', data=dict(id=node_id))
-        rsp = json.loads(rv.data)
-        data = rsp['data']
-        err = rsp['error']
-        self.assertEqual(None, err)
-        self.assertEqual(0, len(data))
+        with rhizi_api.webapp.test_client() as c:
+            req = c.post('/load/node-single', data=dict(id=node_id))
+            req_data = json.loads(req.data)
+            rz_data = req_data['data']
+            rz_err = req_data['error']
+            self.assertEqual(None, rz_err)
+            self.assertEqual(0, len(rz_data))
 
     def test_load_node_existing(self):
         """
-        test loading an existing node
+        loading an existing node test
         """
         node_id = 'skill_00'
-        rv = self.webapp.post('/load/node-single', data=dict(id=node_id))
-        n_set = json.loads(rv.data)['data']
-        
-        self.assertEqual(1, len(n_set))
-        self.assertEqual(n_set[0]['id'], node_id)
+        with rhizi_api.webapp.test_client() as c:
+            req = c.post('/load/node-single', data=dict(id=node_id))
+            n_set = json.loads(req.data)['data']
+
+            self.assertEqual(1, len(n_set))
+            self.assertEqual(n_set[0]['id'], node_id)
 
 if __name__ == "__main__":
     unittest.main()
