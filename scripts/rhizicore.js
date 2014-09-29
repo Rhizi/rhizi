Rhizi/rhizi

commit cd7157955d15802f34dee6ac4578ae249f3e397d

rhizicore: we had a <g> leak, this fixes it
still have new/edited node visible at 0,0 briefly while edited

The fix is by using d3 enter/update/exit and not killing all the nodes
every time we run update.

scripts/rhizicore.js
@@ -377,10 +377,16 @@ function myGraph(el) {
         .attr("width", '100%')
         .attr("height", '100%')
         .attr("pointer-events", "all")
-        .append("g")
         .call(zoomObject)
         .append("g");
 
+    vis.append("rect")
+        .attr("class", "overlay graph")
+        .attr("width", $(el).innerWidth() * 12)
+        .attr("height", $(el).innerHeight() * 12)
+        .attr("x", -$(el).innerWidth() * 5)
+        .attr("y", -$(el).innerHeight() * 5);
+    $('.overlay').click(mousedown);
 
     function zoom() {
         if (graphstate === "GRAPH") {
@@ -494,18 +500,8 @@ function myGraph(el) {
     };
 
     var update = function() {
-        vis.selectAll(".graph").remove();
-
         link = vis.selectAll(".link")
             .data(links);
-
-        vis.append("rect")
-            .attr("class", "overlay graph")
-            .attr("width", $(el).innerWidth()*12)
-            .attr("height", $(el).innerHeight()*12)
-            .attr("x", -$(el).innerWidth()*5)
-            .attr("y", -$(el).innerHeight()*5);
-        $('.overlay').click(mousedown);
 
         link.enter().append("svg:defs").selectAll("marker")
             .data(["end"]) // Different link/path types can be defined here
@@ -538,33 +534,33 @@ function myGraph(el) {
                 //$('#textanalyser').val("node("+d.source.id+") -> "+d.name+" -> node("+d.target.id+")");
             });;
 
+        link.exit().remove();
 
         linktext = vis.selectAll(".linklabel").data(links);
         linktext.enter()
             .append("text")
             .attr("class", "linklabel graph")
             .attr("text-anchor", "middle")
-            .text(function(d) {
+            .on("click", function(d, i) {
+                if(d.state !== "temp") {
+                    editLink(d, i);
+                }
+            });
 
-                if ( d.name)if(d.target.state === "temp" || d.source.state === "chosen" || d.target.state === "chosen") {
+        linktext
+            .text(function(d) {
+                if (d.name) {
                     if (d.name.length < 25 || d.source.state === "chosen" || d.target.state === "chosen" || d.state==="temp") {
                         return d.name;
                     } else {
                         return d.name.substring(0, 14) + "...";
                     }
                 } else {
-                    return " ";
+                    return "";
                 }
-
-            })
-            .on("click", function(d, i) {
-                if(d.state!=="temp")editLink(d, i);
             });
 
-        link.exit().remove();
-
-
-
+        linktext.exit().remove();
 
         node = vis.selectAll(".node")
             .data(nodes, function(d) {
@@ -572,32 +568,35 @@ function myGraph(el) {
             });
 
         var nodeEnter = node.enter()
-            .append("g").call(drag);
-
-
-        nodetext = nodeEnter.insert("text")
-            .attr("class", "nodetext graph")
-            .attr("dx", 15)
-            .attr("dy", ".30em")
-            .text(function(d) {
-                if (d.state === "temp") return d.text;
-                else if (d.state === "chosen") {
-                    return d.text;
-                } else {
-                    if (d.text.length < 28) return d.text;
-                    else return d.text.substring(0, 25) + "...";
-                }
-            })
+            .append("g").attr('class', 'node')
             .on("click", function(d, i) {
                 if(d.state!=="temp"){
                     editNode(d, i);
                     showInfo(d, i);
                 }
+            })
+            .call(drag);
+
+        nodetext = nodeEnter.insert("text")
+            .attr("class", "nodetext graph")
+            .attr("dx", 15)
+            .attr("dy", ".30em");
+
+        node.select('g.node text')
+            .text(function(d) {
+                if (d.state === "temp" || d.state === 'chosen') {
+                     return d.text;
+                } else {
+                    if (d.text.length < 28) {
+                        return d.text;
+                    } else {
+                        return d.text.substring(0, 25) + "...";
+                    }
+                }
             });
 
-
-
-        circle = nodeEnter.insert("circle")
+        circle = nodeEnter.insert("circle");
+        node.select('g.node circle')
             .attr("class", "circle graph")
             .attr("r", function(d) {
                 return customSize(d.type) - 2;
@@ -623,9 +622,12 @@ function myGraph(el) {
                 else return "0 0 0px #FFFF8F";
             })
             .on("click", function(d, i) {
-                 if(d.state!=="temp")showInfo(d, i);
-                else graph.removeHighlight();
-
+                d3.event.stopPropagation();
+                if(d.state!=="temp") {
+                     showInfo(d, i);
+                } else {
+                    graph.removeHighlight();
+                }
                 graph.update();
             });
 

commit 8461b01095284697deda2e755f91212356a09373

rhizicore: bug fix: draw links below nodes
bug introduced in cd7157955d15802f34dee6ac4578ae249f3e397d
before that we recreated the links and nodes every update, and since all
links were created before nodes the document order was
<link>
<link>
<node>
<node>

which per SVG rendering order [1] leads to nodes being drawn on top of
links.

But after that commit the new links and nodes where created as they were
being edited, so we got
<link>
<node>
<link>
<node>

With this commit all links are created in a single group, getting back
the correct order:

<g>
 <link>
 <link>
</g>
<node>
<node>

nodes are ungrouped but there is no need for that (except that it looks
strangely assymetrical).

[1] http://dev.w3.org/SVG/modules/renderorder/SVGRenderOrder.html

scripts/rhizicore.js
@@ -388,6 +388,10 @@ function myGraph(el) {
         .attr("y", -$(el).innerHeight() * 5);
     $('.overlay').click(mousedown);
 
+    // SVG rendering order is last rendered on top, so to make sure
+    // all links are below the nodes we group them under a single g
+    vis.append("g").attr("id", "link-group");
+
     function zoom() {
         if (graphstate === "GRAPH") {
             vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
@@ -500,7 +504,7 @@ function myGraph(el) {
     };
 
     var update = function() {
-        link = vis.selectAll(".link")
+        link = vis.select("#link-group").selectAll(".link")
             .data(links);
 
         link.enter().append("svg:defs").selectAll("marker")