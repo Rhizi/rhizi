386ccba26490590914027f8ab3e64f2bfdd203d9
Alon Levy <alon@pobox.com>
2014-10-19 15:03:42 +0300

    factor out util.js

--- scripts/rhizicore.js
+++ scripts/rhizicore.js
@@ -22,36 +22,6 @@ var scrollValue = 0,
 
 var force;
 
-function set_from_array(a) {
-    var ret = {};
-    for (var k = 0 ; k < a.length ; ++k) {
-        ret[a[k]] = 1;
-    }
-    return ret;
-}
-
-function set_diff(sa, sb) {
-    var ret = {a_b:[], b_a:[]};
-    var i;
-    for (i in sa) {
-        if (!(i in sb)) {
-            ret.a_b.push(i);
-        }
-    }
-    for (i in sb) {
-        if (!(i in sa)) {
-            ret.b_a.push(i);
-        }
-    }
-    return ret;
-}
-
-function array_diff(aa, ab) {
-    var sa = set_from_array(aa);
-    var sb = set_from_array(ab);
-    return set_diff(sa, sb);
-}
-
 function myGraph(el) {
 
     this.update = function(no_relayout) {
---
+++ scripts/util.js
@@ -0,0 +1,35 @@
+function set_from_array(a) {
+    var ret = {};
+    for (var k = 0 ; k < a.length ; ++k) {
+        ret[a[k]] = 1;
+    }
+    return ret;
+}
+
+function set_diff(sa, sb) {
+    var ret = {a_b:[], b_a:[]};
+    var i;
+    for (i in sa) {
+        if (!(i in sb)) {
+            ret.a_b.push(i);
+        }
+    }
+    for (i in sb) {
+        if (!(i in sa)) {
+            ret.b_a.push(i);
+        }
+    }
+    return ret;
+}
+
+function array_diff(aa, ab) {
+    var sa = set_from_array(aa);
+    var sb = set_from_array(ab);
+    return set_diff(sa, sb);
+}
+
+if (typeof(exports) != 'undefined') {
+    exports.set_diff = set_diff;
+    exports.array_diff = array_diff;
+    exports.set_from_array = set_from_array;
+}
