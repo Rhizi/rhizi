82e87d0f203d14097f818f84a27dfc92f267422c
LV-426 <lv-426@taproot.org.il>
2014-11-05 00:38:16 +0200

    broadcast_possible_next_diff_block():    - announce diff block and await commit/reject consensus    - future protocol stub    - currently simply calls backend

--- scripts/rz_api_mesh.js
+++ scripts/rz_api_mesh.js
@@ -1,9 +1,21 @@
 /**
  * API calls designed to execute in decentralized fashion
  */
-define([], function() {
+define(['rz_api_backend'], function(rz_api_backend) {
     function RZ_API_Mesh() {
         
+        /**
+         * suggest diff block and await commit/reject consensus
+         */
+        // @ajax-trans
+        this.broadcast_possible_next_diff_block = function (diff_set) {
+            
+            function on_success(data){
+                // TODO impl
+            }
+            
+            rz_api_backend.commit_diff_set(diff_set);
+        }
     }
     return new RZ_API_Mesh();
 });
