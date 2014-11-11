fcbab7373eb42b9f2053a66babca1abd6dc61709
Alon Levy <alon@pobox.com>
2014-11-11 16:15:39 +0200

    view.hide: must use method access or this is global object

--- scripts/view/view.js
+++ scripts/view/view.js
@@ -1,7 +1,11 @@
+"use strict"
+
 define(['view/node_info', 'view/internal'],
 function(view_node_info,   view_internal) {
 return {
     'node_info': view_node_info,
-    'hide': view_internal.edit_tab.hide,
+    'hide': function() {
+        view_internal.edit_tab.hide();
+    },
 };
 });
