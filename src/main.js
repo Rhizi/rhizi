Rhizi/rhizi

commit af20dd47d3cb254d4dd2638264fe79385a199832

disconnecting model/graph -> textanalysis dep, model should not depend on presenter

src/model/graph.js
@@ -1,7 +1,7 @@
 "use strict"
 
-define(['signal', 'consts', 'util', 'textanalysis', 'model/core', 'model/util', 'rz_api_backend', 'rz_api_mesh'],
-function (signal, consts, util, textanalysis, model_core, model_util, rz_api_backend, rz_api_mesh) {
+define(['signal', 'consts', 'util', 'model/core', 'model/util', 'rz_api_backend', 'rz_api_mesh'],
+function (signal, consts, util, model_core, model_util, rz_api_backend, rz_api_mesh) {
 
 function Graph() {
 
@@ -575,7 +575,7 @@ function Graph() {
                                  x: node.x,
                                  y: node.y,
                                 }, false);
-          textanalysis.autoSuggestAddName(node.name.toLowerCase());
+          // textanalysis.autoSuggestAddName(node.name.toLowerCase());
         }
         for(i = 0; i < data["links"].length; i++){
           link = data.links[i];

commit 61e2b5b87aca0cb5f58d3a41c4e1c649d371e1c8

fix removed textanalysis autoSuggestName
Fix removed textanalysis autoSuggestName call using signal/slot, no
additional signal added (using graph_diff)

Broken in af20dd47d3cb254d4dd2638264fe79385a199832

src/main.js
@@ -1,5 +1,5 @@
-define(['textanalysis.ui', 'buttons', 'history', 'drag_n_drop', 'robot', 'model/core', 'rz_config', 'rz_core'],
-function(textanalysis_ui,   buttons,   history,   drag_n_drop,   robot,   model_core,   rz_config,   rz_core) {
+define(['textanalysis.ui', 'textanalysis', 'buttons', 'history', 'drag_n_drop', 'robot', 'model/core', 'rz_config', 'rz_core'],
+function(textanalysis_ui,   textanalysis,   buttons,   history,   drag_n_drop,   robot,   model_core,   rz_config,   rz_core) {
 
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -33,6 +33,7 @@ function(textanalysis_ui,   buttons,   history,   drag_n_drop,   robot,   model_
         }
         // TODO: interaction between the hack above and this
         model_core.init(rz_config);
+        textanalysis.init();
     }
 
     return {

src/model/graph.js
@@ -575,7 +575,6 @@ function Graph() {
                                  x: node.x,
                                  y: node.y,
                                 }, false);
-          // textanalysis.autoSuggestAddName(node.name.toLowerCase());
         }
         for(i = 0; i < data["links"].length; i++){
           link = data.links[i];

src/textanalysis.js
@@ -1,7 +1,7 @@
 "use strict";
 
-define(['rz_core', 'model/core', 'model/util', 'model/diff'],
-function(rz_core, model_core, model_util, model_diff) {
+define(['rz_core', 'model/core', 'model/util', 'model/diff', 'signal', 'consts'],
+function(rz_core,   model_core,   model_util,   model_diff,   signal,   consts) {
 
 var typeindex = 0;
 var nodetypes = ["person", "project", "skill", "deliverable", "objective"];
@@ -453,7 +453,22 @@ var textAnalyser = function (newtext, finalize) {
     return ret;
 };
 
+function init()
+{
+    function onNodeAdded(diff) {
+        if (diff.nodes.added === undefined) {
+            return;
+        }
+        for (var k in diff.nodes.added) {
+            var node = diff.nodes[k];
+            autoSuggestAddName(node.name.toLowerCase());
+        }
+    }
+    signal.slot(consts.APPLIED_GRAPH_DIFF, onNodeAdded);
+}
+
 return {
+    init:init,
     autocompleteCallback:autocompleteCallback,
     textAnalyser:textAnalyser,
     autoSuggestAddName:autoSuggestAddName,