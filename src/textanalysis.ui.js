486fdb88f10de576c8d0f3d4c5f73072413162c5
Alon Levy <alon@pobox.com>
2014-11-25 22:19:09 +0200

    textanalysis.ui: remove debug prints

--- src/textanalysis.ui.js
+++ src/textanalysis.ui.js
@@ -85,13 +85,11 @@ return {
             triggerEnd: '',
             source: textanalysis.autocompleteCallback,
             response: function(element, ui) {
-                console.log(ui.content);
                 ui.content.forEach(function (x) {
                     if (x.value.search(' ') != -1) {
                         x.value = '"' + x.value + '"';
                     }
                 });
-                console.log(ui.content);
             },
             open: function() {
                 $('.ui-autocomplete').css('width', '10px');
