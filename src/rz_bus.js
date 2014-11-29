4d3fa706644bbf66ee300db5cfaa20a97373483c
Alon Levy <alon@pobox.com>
2014-11-29 22:58:50 +0200

    forgot to add rz_bus

---
+++ src/rz_bus.js
@@ -0,0 +1,15 @@
+"use strict"
+
+define(['consts', 'Bacon'],
+function(consts,   Bacon)
+{
+    var ui_key_bus = new Bacon.Bus(),
+        ui_input_bus = new Bacon.Bus(),
+        names_bus = new Bacon.Bus();
+
+    return {
+        ui_key: ui_key_bus,
+        ui_input: ui_input_bus,
+        names: names_bus,
+    };
+});
