77d81db2ae26fafd65d1fd268b8398c05b18c696
Alon Levy <alon@pobox.com>
2014-12-06 22:08:47 +0200

    Show underscore for empty text to enable click editing (Fixes #190)

--- src/rz_core.js
+++ src/rz_core.js
@@ -471,7 +471,7 @@ function update_view__graph(no_relayout) {
     node.select('g.node text')
         .text(function(d) {
             if (!d.name) {
-                return "";
+                return "_";
             }
             if (d.state === "temp" || d.state === 'chosen'
              || d.state === "enter" || d.state === "exit") {
