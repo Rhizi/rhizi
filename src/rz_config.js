0839ac3ac39c1c16f6eb07250a9724d97c981f5f
LV-426 <lv-426@taproot.org.il>
2014-12-07 17:23:39 +0200

    set 'rhizi.local' as default dev-mode server

--- src/rz_config.js
+++ src/rz_config.js
@@ -2,7 +2,7 @@ define(function() {
 
     return {
         'rand_id_generator' : 'hash',
-        'rz_server_host': 'rhizi.net',
+        'rz_server_host': 'rhizi.local',
         'rz_server_port': '8080',
         'backend_enabled': false,
     };
