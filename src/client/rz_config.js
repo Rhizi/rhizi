411e1bf83cf572b31435d82dcb97d97c2d9526c6
Alon Levy <alon@pobox.com>
2014-12-22 12:05:03 +0200

    client: default to backend enabled - better for testing right now

--- src/client/rz_config.js
+++ src/client/rz_config.js
@@ -4,6 +4,6 @@ define(function() {
         'rand_id_generator' : 'hash',
         'rz_server_host': 'rhizi.local',
         'rz_server_port': '8080',
-        'backend_enabled': false,
+        'backend_enabled': true,
     };
 });
