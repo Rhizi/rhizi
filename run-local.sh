1b2857b853d1e6631f99e5c5f386c4395a116e12
garbash <dor.garbash@gmail.com>
2014-12-22 11:08:49 +0200

    run-local.sh for Mac (Darwin)

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-if [ `(netstat -ltnop 2>/dev/null | grep 7474 | wc -l)` == 0 ]; then
+if [ `uname` == "Darwin" ]; then
+    neo4j_count=`netstat -na -ptcp 2>/dev/null | grep 7474 | wc -l`
+else
+    neo4j_count=`netstat -ltnop 2>/dev/null | grep 7474 | wc -l`
+fi
+if [ $neo4j_count == "0" ]; then
     echo run neo4j please
     exit 0
 fi
