6ffabc45dc18e21c414b18310967c57c1f4600fb
LV-426 <lv-426@taproot.org.il>
2015-01-12 19:10:58 +0200

    update fcgi script

--- src/server/rz_server_fcgi.py
+++ src/server/rz_server_fcgi.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
 
+import cgitb
 from flup.server.fcgi import WSGIServer
 import os
 import sys
-import cgitb
-import rz_server
 
 from rz_kernel import RZ_Kernel
+import rz_server
+
 
 # sys.path.insert(0, '/srv/www/rhizi/rhizi.net/src-py')
-
 # enable debugging
 cgitb.enable()
 
 if __name__ == '__main__':
+
     cfg = rz_server.init_config(cfg_dir='/etc/rhizi')
     log = rz_server.init_log(cfg)
 
     kernel = RZ_Kernel()
     webapp = rz_server.init_webapp(cfg, kernel)
-    rz_server.init_rest_interface(cfg, webapp)
+    ws_srv = rz_server.init_ws_interface(cfg, kernel, webapp)
 
     log.info('launching webapp via flup.server.fcgi.WSGIServer')
 
-    WSGIServer(webapp).run()
+    WSGIServer(ws_srv).run()
