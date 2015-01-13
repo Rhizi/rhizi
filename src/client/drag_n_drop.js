5695d2addf1eb68a4ccbffc4d64afd2f1fb29f33
Alon Levy <alon@pobox.com>
2015-01-13 14:02:14 +0200

    client/drag_n_drop: reindent

--- src/client/drag_n_drop.js
+++ src/client/drag_n_drop.js
@@ -2,29 +2,28 @@ define(['jquery', 'rz_core'], function($, rz_core) {
 
 function init() {
 console.log('rhizi: init drag-n-drop');
-$(document).on('drop', function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    var files = e.originalEvent.dataTransfer.files;
-    var file = files[files.length - 1];
-    var fr = new FileReader();
-    fr.onload = function() {
-        if (fr.readyState != 2) {
-            console.log('drop: error: reading from file failed');
-        } else {
-            console.log('loading dropped file');
-            rz_core.load_from_json(fr.result);
+    $(document).on('drop', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var files = e.originalEvent.dataTransfer.files;
+        var file = files[files.length - 1];
+        var fr = new FileReader();
+        fr.onload = function() {
+            if (fr.readyState != 2) {
+                console.log('drop: error: reading from file failed');
+            } else {
+                console.log('loading dropped file');
+                rz_core.load_from_json(fr.result);
+            }
         }
-    }
-    fr.readAsText(file);
-    return false;
-});
-$(document).on('dragover', function (e) 
-{
-    e.stopPropagation();
-    e.preventDefault();
-    return false;
-});
+        fr.readAsText(file);
+        return false;
+    });
+    $(document).on('dragover', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        return false;
+    });
 };
 
 function prevent_default_drop_behavior() {
