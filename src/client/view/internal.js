cadfc79763a9c5c7473ca8ab764a0f9c56b125c1
Alon Levy <alon@pobox.com>
2015-04-05 21:41:59 +0300

    client: link properties dialog: delete button identical to node dialog

--- src/client/view/internal.js
+++ src/client/view/internal.js
@@ -1,8 +1,8 @@
 define(['view/tab'],
 function(tab) {
 
-var LINK_INFO_SELECTOR = '.link_info',
-    NODE_INFO_SELECTOR = '.info',
+var LINK_INFO_SELECTOR = '#link_info',
+    NODE_INFO_SELECTOR = '#info',
     edit_tab = new tab.Tab({edge: LINK_INFO_SELECTOR, node: NODE_INFO_SELECTOR});
 
 return {
