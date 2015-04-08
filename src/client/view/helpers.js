1ff4cc238422500a7795260c81aa7248f8e9cbaf
Alon Levy <alon@pobox.com>
2015-04-08 09:05:26 +0300

    helpers: remove no longer used customColor (using css right now instead)

--- src/client/view/helpers.js
+++ src/client/view/helpers.js
@@ -1,41 +1,6 @@
 "use strict"
 
 define(function() {
-function customColor(type) {
-    var color;
-    switch (type) {
-        case "person":
-            color = '#FCB924'; //blue
-            break;
-        case "club":
-            color = '#ee3654'; //magenta
-            break;
-        case "skill":
-            color = '#fad900'; //yellow
-            break;
-        case "internship":
-            color = '#ff8b11'; //orange
-            break;
-        case "interest":
-            color = '#8b3ab0'; //purple
-            break;
-        case "project":
-            color = "#40C200"; //green
-            break;
-        case "empty":
-            color = "#919095"; //mid-grey
-            break;
-        case "chainlink":
-            color = "#363636"; //dark-grey
-            break;
-        default:
-            console.log('bug: unknown type ' + type);
-            color = '#d4d4d9'; //mid-light grey
-            break;
-    }
-    return color;
-}
-
 function customSize(type) {
     var size;
     switch (type) {
@@ -70,6 +35,5 @@ function customSize(type) {
 
 return {
     customSize: customSize,
-    customColor: customColor,
 };
 });
