c50c2a32f4f81f47f37397b6091d978587bd1bf7
Alon Levy <alon@pobox.com>
2015-04-10 17:53:38 +0200

    run-local: add a DOMAIN setting for easily testing different domains

--- run-local.sh
+++ run-local.sh
@@ -6,12 +6,13 @@ quit_if_no_neo4j_running
 CONFIG=res/etc/rhizi-server.conf
 EXAMPLE=res/etc/rhizi-server.conf.example
 HTPASSWD=res/etc/htpasswd
+DOMAIN=cri.rhizi.net
 if [ ! -f $CONFIG ]; then
     echo "copying $EXAMPLE to $CONF"
     echo "please edit it as you see fit"
     cp $EXAMPLE $CONFIG
 fi
 make # building css relies on Makefile
-ant -f build.ant deploy-local
+ant -f build.ant deploy-local -DdefaultDomain=$DOMAIN -DtargetDomain=$DOMAIN
 cd deploy-local
 python2.7 bin/rz_server.py --config-dir etc
