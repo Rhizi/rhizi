7bbf33e945848becf2d2e0d7728ce39c7e9744d6
LV-426 <lv-426@taproot.org.il>
2015-04-15 01:19:33 +0300

    rz_api: refactor rz_config construction

--- src/server/rz_api.py
+++ src/server/rz_api.py
@@ -41,9 +41,6 @@ def rz_mainpage(rzdoc_name=None):
             log.exception(e)
             return common_resp_handle__client_error()
 
-    # establish rz_config template values
-    host_addr, host_port = request.host_sock_addr
-
     if None == rzdoc_name:
         s_rzdoc_name = current_app.rz_config.rzdoc__mainpage_name
     else:
@@ -53,11 +50,13 @@ def rz_mainpage(rzdoc_name=None):
             log.exception(e)
             return common_resp_handle__client_error(status=404)
 
-    return render_template('index.html',
-                           rz_username=rz_username,
-                           rz_config__rzdoc_cur__name=s_rzdoc_name,
-                           rz_config__rzdoc_default__name=current_app.rz_config.rzdoc__mainpage_name,
-                           rz_config__hostname=host_addr,
-                           rz_config__port=host_port,
-                           rz_config__optimized_main='true' if current_app.rz_config.optimized_main else 'false',
-                           rz_config__role_set=role_set)
+    # establish rz_config template values
+    host_addr, host_port = request.host_sock_addr[0], request.host_sock_addr[1]
+    rz_config = {'rz_config__rzdoc_cur__name': s_rzdoc_name,
+                 'rz_config__rzdoc__mainpage_name': current_app.rz_config.rzdoc__mainpage_name,
+                 'rz_config__hostname': host_addr,
+                 'rz_config__port': host_port,
+                 'rz_config__optimized_main': 'true' if current_app.rz_config.optimized_main else 'false',
+                 'rz_config__role_set': role_set}
+    return render_template('index.html', rz_username=rz_username, **rz_config)
+
