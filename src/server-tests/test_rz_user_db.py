3ec36778216d29cd69bbf3141ed739fc7499292a
LV-426 <lv-426@taproot.org.il>
2015-04-20 18:46:00 +0300

    test_rz_user_db: update

--- src/server-tests/test_rz_user_db.py
+++ src/server-tests/test_rz_user_db.py
@@ -1,7 +1,8 @@
 import tempfile
 import unittest
-from rz_user_db import User_DB
 
+from rz_user import User_Account
+from rz_user_db import User_DB
 
 class TestUser_DB(unittest.TestCase):
 
@@ -16,12 +17,19 @@ class TestUser_DB(unittest.TestCase):
         tmp_file = tempfile.NamedTemporaryFile(prefix='rz_userdb_', dir='/tmp', suffix='_db')
         tmp_file.close()
 
-        u_name = 'bob'
+        u_first_name = 'bob'
         u_email = 'bob@example.org'
+        pw_hash = ''
+
+        u_account = User_Account(first_name=u_first_name,
+                                 last_name=u_first_name,
+                                 rz_username=u_email,
+                                 email_address=u_email,
+                                 pw_hash=pw_hash)
 
         user_db = User_DB(db_path=tmp_file.name)
         user_db.init(mode='c')
-        uid = user_db.user_add(u_name, u_email)
+        uid = user_db.user_add(u_account)
         user_db.user_add_role(uid, 'admin')
         user_db.shutdown()
 
@@ -32,14 +40,16 @@ class TestUser_DB(unittest.TestCase):
         # lookup_user__by_uid
         ret_uid, ret_u = user_db.lookup_user__by_uid(uid)
         self.assertEqual(ret_uid, uid)
-        self.assertEqual(u_name, ret_u['user_name'])
-        self.assertEqual(u_email, ret_u['email_address'])
+        self.assertEqual(u_email, ret_u.email_address)
+        self.assertEqual(u_email, ret_u.rz_username)
+        self.assertEqual(u_first_name, ret_u.first_name)
+        self.assertFalse(hasattr(ret_u, 'pw_hash'))
 
         # lookup_user__by_username
         ret_uid, ret_u = user_db.lookup_user__by_email_address(u_email)
         self.assertEqual(ret_uid, uid)
-        self.assertEqual(u_name, ret_u['user_name'])
-        self.assertEqual(u_email, ret_u['email_address'])
+        self.assertEqual(u_email, ret_u.rz_username)
+        self.assertEqual(u_email, ret_u.email_address)
 
         user_db.dump_to_file__str()
         user_db.shutdown()
