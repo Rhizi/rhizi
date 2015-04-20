11a9c68b8133266591eea1798c7d509e56f23bbb
garbash <dor.garbash@gmail.com>
2015-04-20 13:31:46 +0300

    Added domain type for Descartes instance

---
+++ src/local/domain/descartes.rhizi.net/domain_types.js
@@ -0,0 +1,55 @@
+define({
+    type_attributes: [
+        ['person', {
+            'title': 'Person',
+            'attributes': ['description',
+                           'email',
+                           'image-url',
+                           'name',
+                           'url'
+                           ],
+        }],
+        ['beneficiary', {
+            'title': 'Beneficiary',
+            'attributes': ['description', 'url', 'image-url', 'email'],
+        }],
+        ['completed project', {
+            'title': 'Completed project',
+            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
+        }],
+        ['ongoing project', {
+            'title': 'Ongoing project project',
+            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
+        }],
+        ['future project', {
+            'title': 'Future project',
+            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
+        }],
+        ['skill', {
+            'title': 'Skill',
+            'attributes': ['description', 'url', 'image-url'],
+        }]
+    ],
+    attribute_titles: {
+        'description': 'Description',
+        'email': 'Email',
+        'name': 'Name',
+        'type': 'Type',
+        'url': 'URL',
+        'image-url': 'Image URL',
+        'rhizi-url': 'Rhizi URL',
+        'total days': 'Total days',
+        'budget': 'Budget',
+    },
+    attribute_ui: {
+        'description': 'textarea',
+        'email': 'input',
+        'name': 'input',
+        'type': 'type',
+        'image-url': 'image',
+        'url': 'url',
+        'rhizi-url': 'rhizi-url',
+        'total days': 'input',
+        'budget': 'input',
+    },
+});
