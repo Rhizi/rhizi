15629cbc1eeb7cef228307a1d796a3d418f059bf
Alon Levy <alon@pobox.com>
2015-04-23 21:45:09 +0300

    docker-run: fix for new path

--- src/local/docker/docker-build.sh
+++ src/local/docker/docker-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+ROOT_DOCKER_DIR=$(dirname $(realpath $0))
+FEDORA_DOCKER=$ROOT_DOCKER_DIR/fedora
 if [ ! -e .git ]; then
     echo run from top level checkout of rhizi repository
     exit 0
 fi
-if [ ! -e docker/fedora/neo4j-community-2.2.1-unix.tar.gz ]; then
+NEO4J_TARBALL=$FEDORA_DOCKER/neo4j-community-2.2.1-unix.tar.gz
+if [ ! -e $NEO4J_TARBALL ]; then
     echo downloading neo4j community edition 2.2.1
-    wget http://neo4j.com/artifact.php?name=neo4j-community-2.2.1-unix.tar.gz -Odocker/fedora/neo4j-community-2.2.1-unix.tar.gz
+    wget http://neo4j.com/artifact.php?name=neo4j-community-2.2.1-unix.tar.gz -O$NEO4J_TARBALL
 fi
-docker build -t alon/rhizi:test1 docker/fedora
+docker build -t alon/rhizi:test1 $FEDORA_DOCKER
