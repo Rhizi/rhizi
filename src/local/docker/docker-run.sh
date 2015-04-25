ad0685823df03d09b1e498295ca7174d2678b173
Alon Levy <alon@pobox.com>
2015-04-25 23:46:22 +0300

    docker/docker-run.sh: split long line

--- src/local/docker/docker-run.sh
+++ src/local/docker/docker-run.sh
@@ -11,8 +11,9 @@ fi
 CONFIG_FILE=etc/rhizi-server.conf
 SED_1='s|neo4j_url.*|neo4j_url = http:\/\/$N4_PORT_7474_TCP_ADDR:7474|'
 SED_2='s|listen_address.*|listen_address = 0.0.0.0|'
-
 (
     ant -f build.ant deploy-local -DlocalServerUseSymlink=false &&
-    docker run --name rhizi.devel.$PORT -it -p $PORT:8080 --link rhizi.neo4j:n4 -v `pwd`/deploy-local:/home/rhizi rhizi/fedora:0.1 bash -c "sed -ie \"$SED_1\" $CONFIG_FILE; sed -ie \"$SED_2\" $CONFIG_FILE; python2.7 bin/rz_server.py --config-dir etc"
+    docker run --name rhizi.devel.$PORT -it -p $PORT:8080 --link rhizi.neo4j:n4 -v `pwd`/deploy-local:/home/rhizi rhizi/fedora:0.1 bash -c "\
+        sed -ie \"$SED_1\" -e \"$SED_2\" $CONFIG_FILE;\
+        python2.7 bin/rz_server.py --config-dir etc"
 )
