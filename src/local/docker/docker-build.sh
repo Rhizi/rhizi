0a20324e01210b6654c5f4415efd53d44c2c5246
Alon Levy <alon@pobox.com>
2015-04-25 16:56:34 +0300

    docker: move Dockerfile to res/docker, leave scripts in src/local/docker

--- src/local/docker/docker-build.sh
+++ src/local/docker/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROOT_DOCKER_DIR=$(dirname $(realpath $0))
+ROOT_DOCKER_DIR=$(dirname $(realpath $0))/../../../res/docker/
 FEDORA_DOCKER=$ROOT_DOCKER_DIR/fedora
 if [ ! -e .git ]; then
     echo run from top level checkout of rhizi repository
