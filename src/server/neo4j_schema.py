f7cac6aeb4031cb40bb6b0edb9d0c99a030a1d9e
LV-426 <lv-426@taproot.org.il>
2015-04-29 19:05:44 +0300

    neo4j_schema: META_LABEL__RZDB_META

--- src/server/neo4j_schema.py
+++ src/server/neo4j_schema.py
@@ -1,17 +1,19 @@
 #
 # meta label schema
 #
-META_LABEL__RZDOC_TYPE = '__RZDOC' # RZdoc node
+META_LABEL__RZDB_META = '__RZDB_META'
+
+META_LABEL__RZDOC_TYPE = '__RZDOC'  # RZdoc node
 META_LABEL__RZDOC_NS_PREFIX = '__RZDOC_NS_'
 META_LABEL__RZDOC_NS_META_PREFIX = '__RZDOC_NS_META_'
 
 META_LABEL__VC_HEAD = '__HEAD'
-META_LABEL__VC_COMMIT = '__COMMIT' # diff commit node
-META_LABEL__VC_PARENT = '__PARENT' # diff commit node
+META_LABEL__VC_COMMIT = '__COMMIT'  # diff commit node
+META_LABEL__VC_PARENT = '__PARENT'  # diff commit node
 META_LABEL__VC_COMMIT_AUTHOR = '__AUTHORED-BY'
-META_LABEL__VC_EMPTY_RZDOC_HASH = 'adc83b19e793491b1c6ea0fd8b46cd9f32e592fc' # value of: echo '' | sha1sum
+META_LABEL__VC_EMPTY_RZDOC_HASH = 'adc83b19e793491b1c6ea0fd8b46cd9f32e592fc'  # value of: echo '' | sha1sum
 
-META_LABEL__USER = '__USER' # user node
+META_LABEL__USER = '__USER'  # user node
 
 meta_label_set = [META_LABEL__VC_HEAD,
                   META_LABEL__VC_COMMIT,
