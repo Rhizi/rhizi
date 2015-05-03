4fb6f35ec9255f812ef1c1e6ddb3018696022adb
garbash <dor.garbash@gmail.com>
2015-05-03 17:40:29 +0300

    Updates domain_types.js for descartes instance

--- src/local/domain/descartes.rhizi.net/domain_types.js
+++ src/local/domain/descartes.rhizi.net/domain_types.js
@@ -1,53 +1,47 @@
 define({
-    type_attributes: [
+    type_attributes:[
         ['person', {
-            'title': 'Person',
+            'title':'Personne',
             'attributes': ['description',
                            'email',
                            'image-url',
                            'name',
                            'url'
                            ],
-        }],
-        ['beneficiary', {
-            'title': 'Beneficiary',
-            'attributes': ['description', 'url', 'image-url', 'email'],
-        }],
-        ['completed-project', {
-            'title': 'Completed project',
-            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
-        }],
-        ['ongoing-project', {
-            'title': 'Ongoing project project',
-            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
-        }],
-        ['future-project', {
-            'title': 'Future project',
-            'attributes': ['description', 'url', 'budget', 'total days','image-url'],
-        }],
+            }],
+        ['organisation', {
+            'title':'Organisation',
+            'attributes':['description', 'url', 'image-url', 'email'],
+            }],
+        ['project', {
+            'title':'Projet',
+            'attributes':['description', 'status', 'url', 'budget', 'total days','image-url'],
+            }],
         ['skill', {
-            'title': 'Skill',
-            'attributes': ['description', 'url', 'image-url'],
+        'title':'Compétence',
+        'attributes':['description', 'url', 'image-url'],
         }]
     ],
-    attribute_titles: {
-        'description': 'Description',
-        'email': 'Email',
-        'name': 'Name',
-        'type': 'Type',
-        'url': 'URL',
-        'image-url': 'Image URL',
-        'total days': 'Total days',
-        'budget': 'Budget',
-    },
-    attribute_ui: {
-        'description': 'textarea',
-        'email': 'input',
-        'name': 'input',
-        'type': 'type',
-        'image-url': 'image',
-        'url': 'url',
-        'total days': 'total days',
-        'budget': 'input',
-    },
-});
+    attribute_titles:{
+        'description':'Description',
+        'email':'Email',
+        'name':'Nom',
+        'type':'Type',
+        'url':'URL',
+        'image-url':'Image URL',
+        'total days':'Jours travaillés',
+        'budget':'Budget en k€',
+        'status':'Statut'
+        },
+        attribute_ui:{
+        'description':'textarea',
+        'email':'input',
+        'name':'input',
+        'type':'type',
+        'image-url':'image',
+        'url':'url',
+        'total days':'total days',
+        'budget':'input',
+        'status':'input'
+        },
+    });
