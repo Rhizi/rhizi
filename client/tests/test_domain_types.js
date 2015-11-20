32056ce655b37275a7c99e23af4dfc3f014dad28
Alon Levy <alon@pobox.com>
2015-11-20 14:12:45 +0200

    test_domain_types: add basic model/types test

--- client/tests/test_domain_types.js
+++ client/tests/test_domain_types.js
@@ -1,9 +1,14 @@
-require(['underscore', 'domain_types'],
-function(_, domain_types) {
+require(['underscore', 'domain_types', 'model/types'],
+function(_, domain_types, model_types) {
     'use strict';
     describe("domain_types", function () {
         it("sanity test for domain_types", function () {
             expect(_.keys(domain_types)).toEqual(["types", "attribute_titles", "attribute_ui", "misc"]);
+        });
+    });
+    describe("model/types", function () {
+        it("sanity test for model/types", function () {
+            expect(_.isArray(model_types.nodetypes)).toBe(true);
         });
     });
 });
