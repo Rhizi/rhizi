7d3fb0b8e1b4fcc117bd6e2c91e3f757da77aebf
Clément Renaud <clement.renaud@gmail.com>
2015-12-04 19:58:41 +0100

    fix edge head arrows

--- client/rz_core.js
+++ client/rz_core.js
@@ -148,13 +148,16 @@ var init_graph_views = function () {
         .append("svg:marker") // This section adds in the arrows
         .attr("id", String)
         .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 22)
-        .attr("refY", -1.5)
-        .attr("markerWidth", 4)
-        .attr("markerHeight", 4)
+        .attr("refX", 30)
+        .attr("refY", 0)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
+        .style("fill", "#AAA")
+        .attr("markerUnits", "userSpaceOnUse") // default: "strokeWidth"
         .attr("orient", "auto")
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
+
 
     /*
      * init zoom behavior
