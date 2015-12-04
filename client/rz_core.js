8a49c96628bc1c78f254429967f55bc71d73f5e2
Clément Renaud <clement.renaud@gmail.com>
2015-12-04 19:44:54 +0100

    Revert "client/rz_core: tweak the end marker to work with the new variable width - make the marker size a constant unrelated to stroke-width"
    This reverts commit e34b2f9b4b28c3699b6199d1dfdc4fb8c0355e86.

--- client/rz_core.js
+++ client/rz_core.js
@@ -148,11 +148,10 @@ var init_graph_views = function () {
         .append("svg:marker") // This section adds in the arrows
         .attr("id", String)
         .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 30)
-        .attr("refY", 0)
-        .attr("markerWidth", 8)
-        .attr("markerHeight", 8)
-        .attr("markerUnits", "userSpaceOnUse") // default: "strokeWidth"
+        .attr("refX", 22)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 4)
+        .attr("markerHeight", 4)
         .attr("orient", "auto")
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
