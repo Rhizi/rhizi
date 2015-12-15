bc55d303b5b2a31119a039d6f3c5f265e6c4e237
Alon Levy <alon@pobox.com>
2015-12-15 10:10:50 -0800

    client/keyshortcuts.js: remove explicit selection.related_nodes

--- client/keyshortcuts.js
+++ client/keyshortcuts.js
@@ -17,8 +17,8 @@
 */
 
 define('keyshortcuts',
-       ['jquery', 'rz_core', 'view/selection', 'view/item_info'],
-function($,        rz_core,        selection,        item_info) {
+       ['jquery', 'Bacon', 'rz_core', 'view/selection', 'view/item_info'],
+function($,        Bacon,   rz_core,        selection,        item_info) {
 
 "use strict";
 
@@ -32,55 +32,64 @@ function get_graph_view(element)
     return element !== undefined && element !== null ? dict[element.id] : undefined;
 }
 
-function install() {
-    document.body.onkeydown = function(e) {
-        var keyBase = ((e.key && String(e.key))                         ||
-                      (e.charCode && String.fromCharCode(e.charCode))   ||
-                      (e.which && String.fromCharCode(e.which))),
-            key = (keyBase || "").toLowerCase(),
-            handled = false,
-            graph_view = get_graph_view(e.target);
+function on_key_and_selection(stream) {
+    var e = stream[0],
+        cur_selection = stream[1],
+        keyBase = ((e.key && String(e.key))                         ||
+                  (e.charCode && String.fromCharCode(e.charCode))   ||
+                  (e.which && String.fromCharCode(e.which))),
+        key = (keyBase || "").toLowerCase(),
+        handled = false,
+        graph_view = get_graph_view(e.target);
 
-        if (!e.altKey && !e.ctrlKey && e.keyCode === 46) { // del
-            selection.delete_selection(rz_core.main_graph);
+    if (!e.altKey && !e.ctrlKey && e.keyCode === 46) { // del
+        selection.delete_selection(rz_core.main_graph);
+    }
+    if (!e.altKey && !e.ctrlKey && e.keyCode === 27) { // escape
+        item_info.hide();
+    }
+    if (e.altKey && e.ctrlKey && 'i' === key) {
+        $('#textanalyser').focus();
+    }
+    if (e.ctrlKey && '9' === key) {
+        rz_core.main_graph_view.nodes__user_visible(cur_selection.related_nodes, true);
+        handled = true;
+    }
+    if (e.ctrlKey && '0' === key) {
+        if (!cur_selection.is_empty()) {
+            rz_core.main_graph_view.zen_mode__toggle();
         }
-        if (!e.altKey && !e.ctrlKey && e.keyCode === 27) { // escape
-            item_info.hide();
-        }
-        if (e.altKey && e.ctrlKey && 'i' === key) {
-            $('#textanalyser').focus();
-        }
-        if (e.ctrlKey && '9' === key) {
-            rz_core.main_graph_view.nodes__user_visible(selection.related_nodes(), true);
-            handled = true;
-        }
-        if (e.ctrlKey && '0' === key) {
-            if (!selection.is_empty()) {
-                rz_core.main_graph_view.zen_mode__toggle();
-            }
-            handled = true;
-        }
-        if (e.altKey && e.ctrlKey && 'o' === key) {
-            $("#search").focus();
-            handled = true;
-        }
-        if (e.ctrlKey && 'a' === key && e.target === document.body) {
-            selection.select_nodes(rz_core.main_graph.nodes());
-            handled = true;
-        }
-        if (e.ctrlKey && 'z' === key && e.target.nodeName !== 'INPUT') {
-            // TODO: rz_core.main_graph.undo();
-        }
-        // SVG elements cannot handle any keys directly - pass the key to them in this case
-        if (undefined !== graph_view) {
-            graph_view.keyboard_handler(e);
-        }
-        if (handled) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-    };
+        handled = true;
+    }
+    if (e.altKey && e.ctrlKey && 'o' === key) {
+        $("#search").focus();
+        handled = true;
+    }
+    if (e.ctrlKey && 'a' === key && e.target === document.body) {
+        selection.select_nodes(rz_core.main_graph.nodes());
+        handled = true;
+    }
+    if (e.ctrlKey && 'z' === key && e.target.nodeName !== 'INPUT') {
+        // TODO: rz_core.main_graph.undo();
+    }
+    // SVG elements cannot handle any keys directly - pass the key to them in this case
+    if (undefined !== graph_view) {
+        graph_view.keyboard_handler(e);
+    }
+    if (handled) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
 }
+
+function install() {
+    selection.selection
+        .sampledBy(Bacon.fromEvent(document.body, 'keydown'), function (selection, key_event) {
+            return [key_event, selection];
+        })
+        .onValue(on_key_and_selection);
+}
+
 return {
         install: install
        };
